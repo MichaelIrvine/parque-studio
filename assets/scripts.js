4887dd442ee648cc7fa2bac1a87ccff6bdbf61dd
Michael Irvine <michaelirvinedesign@gmail.com>
2021-03-10 13:02:22 -0800

    tidying up mobile

--- assets/scripts.js
+++ assets/scripts.js
@@ -8216,7 +8216,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "55638" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "64870" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
